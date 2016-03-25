4692ce5cf4714586e40ac59cd56cf618b1b56ab0
erissnguyenn <erissnguyenn@gmail.com>
2016-03-24 21:59:20 -0400

    Created SwapName program; added to menu

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace NguyenMaiLy_Assignment05
 {
@@ -16,32 +16,68 @@ namespace NguyenMaiLy_Assignment05
 			 */
 
 			Console.WriteLine ("===================================================================");
-			Console.WriteLine ("                            Swap Name                              ");
+			Console.WriteLine ("                               MENU                                ");
 			Console.WriteLine ("===================================================================");
 
-			// Prompt user for first name
-			Console.Write ("Please enter in your FIRST name: ");
-			// Get input for first name
-			string firstName_Str = Console.ReadLine ();
+			Console.WriteLine ("Please choose a program:");
+			Console.WriteLine ("  A. Swap Name");
+			Console.WriteLine ("  B. Backwards");
+			Console.WriteLine ("  C. ...");
+			Console.WriteLine ("  D. ...");
+			Console.Write ("Please enter only A, B, C, or D: ");
+			string response = Console.ReadLine ();
+			if (response == "A" || response == "a"){
 
-			// Prompt user for last name
-			Console.Write ("Please enter in your LAST name: ");
-			// Get input for last name
-			string lastName_Str = Console.ReadLine ();
+				Console.WriteLine ("===================================================================");
+				Console.WriteLine ("                       Exercise 1: Swap Name                       ");
+				Console.WriteLine ("===================================================================");
 
-			// Call function SwapName()
-			SwapName (firstName_Str, lastName_Str);
+				// Prompt user for first name
+				Console.Write ("Please enter in your FIRST name: ");
+				// Get input for first name
+				string firstName_Str = Console.ReadLine ();
+
+				// Prompt user for last name
+				Console.Write ("Please enter in your LAST name: ");
+				// Get input for last name
+				string lastName_Str = Console.ReadLine ();
+
+				// Call function SwapName()
+				SwapName (firstName_Str, lastName_Str);
+			}
+
+
+		
+
+
+
+
+			Console.WriteLine ("===================================================================");
+			Console.WriteLine ("                     Exercise 2: Backwards                         ");
+			Console.WriteLine ("===================================================================");
+
+			Console.WriteLine ("Please enter a sentence that has at least SIX words in it: ");
+			string sentence_Str = Console.ReadLine ();
+
+			Console.WriteLine ("The sentence you entered: " + sentence_Str);
+
+
 		}
+
 
 		// FUNCTIONS ---------------------------------------------------------------------------------
 
 		// Exercise 1: Swap Name
 		public static string SwapName(string firstName, string lastName){
+
 			Console.WriteLine ("\r\nYour FIRST and LAST name: " + firstName + " " + lastName);
 			Console.WriteLine ("Your SWAPPED FIRST name: " + lastName);
 			Console.WriteLine ("Your SWAPPED LAST name: " + firstName);
 
 			return null;
 		}
+
+		// Exercise 2: Backwards
+
 	}
 }
