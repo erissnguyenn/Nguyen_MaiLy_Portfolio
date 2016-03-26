7703cf1e50821d1d9a0d76b9029abeea581b2746
erissnguyenn <erissnguyenn@gmail.com>
2016-03-25 23:48:30 -0400

    FINISHED PROGRAM
    Completed description and added MainMenu function

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace NguyenMaiLy_Assignment05
 			 * Project and Portfolio I: Development
 			 * Assignment 05: Building a Code Repository
 			 * 
-			 * DESCRIPTION: BLAH BLAH
+			 * DESCRIPTION: This project consists of a menu and 4 programs: SwapName, Backwards, AgeConvert, and TempConvert.
 			 */
 
 			Console.WriteLine ("===================================================================");
@@ -65,6 +65,9 @@ namespace NguyenMaiLy_Assignment05
 					// Call function SwapName()
 					SwapName (firstName_Str, lastName_Str);
 
+					// Call function MainMenu()
+					MainMenu(response);
+
 					break; 
 
 				} else if (response == "B" || response == "b") {
@@ -79,6 +82,9 @@ namespace NguyenMaiLy_Assignment05
 
 					// Call function Backwards()
 					Backwards (sentence_Str);
+
+					// Call function MainMenu()
+					MainMenu(response);
 					
 					break;
 
@@ -119,6 +125,9 @@ namespace NguyenMaiLy_Assignment05
 
 					// Call function AgeConvert()
 					AgeConvert(name_Str, age);
+
+					// Call function MainMenu()
+					MainMenu(response);
 
 					break;
 
@@ -168,6 +177,9 @@ namespace NguyenMaiLy_Assignment05
 
 					// Call function TempConvert();
 					TempConvert(fahrenheit, celsius);
+
+					// Call function MainMenu()
+					MainMenu(response);
 
 					break;
 
@@ -219,6 +231,9 @@ namespace NguyenMaiLy_Assignment05
 						// Remove any punctuations in reversed output
 						Console.Write (word.Trim ('.').Trim ('!').Trim ('?') + " ");
 					}
+
+					Console.WriteLine ("\r\n");
+
 					break;
 
 				} else {
@@ -277,6 +292,186 @@ namespace NguyenMaiLy_Assignment05
 			Console.WriteLine (celsius + "° Celsius --> " + Math.Round(fahrenheitValue, 2) + "° Fahrenheit");
 
 			return null;
+		}
+
+		// Main Menu -----------------------------------------------------------------------------------
+		public static string MainMenu(string userResponse){
+			Console.WriteLine ("===================================================================");
+			Console.WriteLine ("                               MENU                                ");
+			Console.WriteLine ("===================================================================");
+
+			Console.WriteLine ("Please choose a program:");
+			Console.WriteLine ("  A. Swap Name");
+			Console.WriteLine ("  B. Backwards");
+			Console.WriteLine ("  C. AgeConvert");
+			Console.WriteLine ("  D. TempConvert");
+			Console.Write ("\r\nPlease enter only A, B, C, or D: ");
+			string response = Console.ReadLine ();
+			// Check to see if user entered anything;
+			// If user leave blank, output error message and prompt again.
+			while (string.IsNullOrWhiteSpace (response)) {
+				Console.Write ("\r\n!! ERROR !! PLEASE DO NOT LEAVE THIS PART BLANK !!\r\n--> Please enter in your response using only A, B, C or D: ");
+				response = Console.ReadLine ();
+			}
+
+			while (true) {
+				if (response == "A" || response == "a") {
+
+					Console.WriteLine ("===================================================================");
+					Console.WriteLine ("                       Exercise 1: SwapName                        ");
+					Console.WriteLine ("===================================================================");
+
+					// Prompt user for FIRST NAME
+					Console.Write ("Please enter in your FIRST name: ");
+					// Get input for first name
+					string firstName_Str = Console.ReadLine ();
+					// Check to see if user entered anything;
+					// If user leave blank, output error message and prompt again.
+					while (string.IsNullOrWhiteSpace (firstName_Str)) {
+						Console.Write ("\r\n!! ERROR !! PLEASE DO NOT LEAVE THIS PART BLANK !!\r\n--> Please enter in your FIRST name: ");
+						firstName_Str = Console.ReadLine ();
+					}
+
+					// Prompt user for LAST NAME
+					Console.Write ("\r\nPlease enter in your LAST name: ");
+					// Get input for last name
+					string lastName_Str = Console.ReadLine ();
+					// Check to see if user entered anything;
+					// If user leave blank, output error message and prompt again.
+					while (string.IsNullOrWhiteSpace (lastName_Str)) {
+						Console.Write ("\r\n!! ERROR !! PLEASE DO NOT LEAVE THIS PART BLANK !!\r\n--> Please enter in your LAST name: ");
+						lastName_Str = Console.ReadLine ();
+					}
+
+					// Call function SwapName()
+					SwapName (firstName_Str, lastName_Str);
+
+					// Call function MainMenu()
+					MainMenu(response);
+
+					break; 
+
+				} else if (response == "B" || response == "b") {
+					Console.WriteLine ("===================================================================");
+					Console.WriteLine ("                       Exercise 2: Backwards                       ");
+					Console.WriteLine ("===================================================================");
+
+					// Prompt user for sentence
+					Console.WriteLine ("Please enter a sentence that has at least SIX words in it: ");
+					// Get input for sentence
+					string sentence_Str = Console.ReadLine ();
+
+					// Call function Backwards()
+					Backwards (sentence_Str);
+
+					// Call function MainMenu()
+					MainMenu(response);
+
+					break;
+
+				} else if (response == "C" || response == "c") {
+					Console.WriteLine ("===================================================================");
+					Console.WriteLine ("                       Exercise 3: AgeConvert                      ");
+					Console.WriteLine ("===================================================================");
+
+					// Prompt user for name
+					Console.Write ("Please enter in your NAME: ");
+					// Get input for name
+					string name_Str = Console.ReadLine ();
+					// Check to see if user entered anything;
+					// If user leave blank, output error message and prompt again.
+					while (string.IsNullOrWhiteSpace (name_Str)) {
+						Console.Write ("\r\n!! ERROR !! PLEASE DO NOT LEAVE THIS PART BLANK !!\r\n--> Please enter in your NAME: ");
+						name_Str = Console.ReadLine ();
+					}
+
+					// Prompt user for age
+					Console.Write ("\r\nPlease enter in your AGE: ");
+					// Get input for age
+					string age_Str = Console.ReadLine ();
+					// Check to see if user entered anything;
+					// If user leave blank, output error message and prompt again.
+					while (string.IsNullOrWhiteSpace (age_Str)) {
+						Console.Write ("\r\n!! ERROR !! PLEASE DO NOT LEAVE THIS PART BLANK !!\r\n--> Please enter in your AGE: ");
+						age_Str = Console.ReadLine ();
+					}
+					// Check to see if input is a number, if not try again.
+					int age = 0;
+
+					while(!int.TryParse(age_Str, out age)){
+						// Prompt user invalid entry and reprompt them for their age 
+						Console.Write ("\r\n!! ERROR !! INVALID ENTRY !!\r\n--> Please enter your age: ");
+						age_Str = Console.ReadLine();
+					}
+
+					// Call function AgeConvert()
+					AgeConvert(name_Str, age);
+
+					// Call function MainMenu()
+					MainMenu(response);
+
+					break;
+
+				} else if (response == "D" || response == "d") {
+					Console.WriteLine ("===================================================================");
+					Console.WriteLine ("                       Exercise 4: TempConvert                     ");
+					Console.WriteLine ("===================================================================");
+
+					// Prompt user for temperature in Fahrenheit
+					Console.Write ("Please enter in the temperature NUMBER in FAHRENHEIT: ");
+					// Get input for temperature in Fahrenheit
+					string fahrenheit_Str = Console.ReadLine ();
+					// Check to see if user entered anything;
+					// If user leave blank, output error message and prompt again.
+					while (string.IsNullOrWhiteSpace (fahrenheit_Str)) {
+						Console.Write ("\r\n!! ERROR !! PLEASE DO NOT LEAVE THIS PART BLANK !!\r\n--> Please enter in the temperature NUMBER in FAHRENHEIT: ");
+						fahrenheit_Str = Console.ReadLine ();
+					}
+					// Check to see if input is a number, if not try again.
+					double fahrenheit = 0;
+
+					while(!double.TryParse(fahrenheit_Str, out fahrenheit)){
+						// Prompt user invalid entry and reprompt them for the number in Fahrenheit
+						Console.Write ("\r\n!! ERROR !! INVALID ENTRY !!\r\n--> Please enter in the temperature number in FAHRENHEIT: ");
+						fahrenheit_Str = Console.ReadLine();
+					}
+
+
+					// Prompt user for temperature in Celsius
+					Console.Write ("Please enter in the temperature NUMBER in CELSIUS: ");
+					// Get input for temperature in Celsius
+					string celsius_Str = Console.ReadLine ();
+					// Check to see if user entered anything;
+					// If user leave blank, output error message and prompt again.
+					while (string.IsNullOrWhiteSpace (celsius_Str)) {
+						Console.Write ("\r\n!! ERROR !! PLEASE DO NOT LEAVE THIS PART BLANK !!\r\n--> Please enter in the temperature NUMBER in CELSIUS: ");
+						celsius_Str = Console.ReadLine ();
+					}
+					// Check to see if input is a number, if not try again.
+					double celsius = 0;
+
+					while(!double.TryParse(celsius_Str, out celsius)){
+						// Prompt user invalid entry and reprompt them for the number in Celsius
+						Console.Write ("\r\n!! ERROR !! INVALID ENTRY !!\r\n--> Please enter in the temperature number in CELSIUS: ");
+						celsius_Str = Console.ReadLine();
+					}
+
+					// Call function TempConvert();
+					TempConvert(fahrenheit, celsius);
+
+					// Call function MainMenu()
+					MainMenu(response);
+
+					break;
+
+				} else {
+					Console.Write ("\r\n!! ERROR !! INVALID ENTRY !!\r\n--> Please enter in your response using only A, B, C or D: ");
+					response = Console.ReadLine ();
+				}
+			}
+
+			return null;
+
 		}
 	}
 }
